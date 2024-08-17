a8b6c0285a528f0c19e4c79b1ecd77653296f590
Daniel McInnis <dmcinnis@uci.edu>
2024-08-16 20:48:37 -0700

    Refine wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,16 +2,17 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.enable_tab_bar = true
+config.color_scheme = 'Vs Code Dark+ (Gogh)'
 
-config.font = wezterm.font({ family = 'Berkeley Mono'})
-config.font_size = 16
+config.font = wezterm.font({ family = 'Berkeley Mono' })
+config.font_size = 18
 
 config.window_frame = {
   font = wezterm.font({ family = 'Berkeley Mono', weight = 'Bold' }),
-  font_size = 16,
+  font_size = 18,
 }
 
-config.window_decorations = 'RESIZE|INTEGRATED_BUTTONS'
+config.window_decorations = 'RESIZE'
 config.window_close_confirmation = 'NeverPrompt'
 
 return config
