e6b9cfa1690f123824a0d4703c42c2242f71195b
Daniel McInnis <dmcinnis@uci.edu>
2024-08-17 09:18:33 -0700

    [WezTerm] Status bar additions

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,8 +1,10 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.enable_tab_bar = true
 config.color_scheme = 'Vs Code Dark+ (Gogh)'
+
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 30
 
 config.font = wezterm.font({ family = 'Berkeley Mono' })
 config.font_size = 18
@@ -25,5 +27,23 @@ config.keys = {
     action = wezterm.action.ToggleFullScreen,
   },
 }
+
+
+wezterm.on('update-status', function(window)
+  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+  local color_scheme = window:effective_config().resolved_palette
+  local bg = color_scheme.background
+  local fg = color_scheme.foreground
+
+  window:set_right_status(wezterm.format({
+    { Background = { Color = 'none' } },
+    { Foreground = { Color = bg } },
+    { Text = SOLID_LEFT_ARROW },
+    { Background = { Color = bg } },
+    { Foreground = { Color = fg } },
+    { Text = ' ' .. wezterm.strftime('%a, %b %-d | %H:%M') .. ' ' },
+  }))
+end)
+
 
 return config
